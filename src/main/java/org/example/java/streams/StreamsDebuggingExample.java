e7aa431d9eed33d360c8595f89b95bf2c803a2fd
Andrew Ray Abad <dev.arabad@gmail.com>
2021-03-18 11:25:14 +0800

    [update] [20210318] [ara] - Rename file

--- src/main/java/org/example/java/streams/StreamsDebuggingExample.java
+++ src/main/java/org/example/java/streams/StreamsDebuggingExample.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class StreamDebuggingExample
+public class StreamsDebuggingExample
 {
   public static void main(String[] args)
   {
